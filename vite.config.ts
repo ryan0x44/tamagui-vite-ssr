bddf8b457bad4c8d05ddba277e8dcd34a2cb0213
Ryan D <git@ryan0x44.com>
2023-06-28 16:42:34 +1000

    Added tamagui config to vite.config.ts Per https://tamagui.dev/docs/guides/vite#installation

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { tamaguiExtractPlugin, tamaguiPlugin } from '@tamagui/vite-plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tamaguiPlugin({
+      config: 'tamagui.config.ts',
+      components: ['tamagui'],
+    }),
+  ].filter(Boolean),
 })

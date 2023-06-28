753ab06de1e9ee21b584e95664b299c2b1f6f5c5
Ryan D <git@ryan0x44.com>
2023-06-28 17:01:33 +1000

    Removed boilerplate from src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import '@tamagui/core/reset.css'
 import { Suspense, useState } from 'react'
-import { TamaguiProvider } from 'tamagui'
+import { TamaguiProvider, YStack, H1, Button } from 'tamagui'
 import tamaguiConfig from '../tamagui.config'
 
 function App() {
@@ -11,26 +11,10 @@ function App() {
   return (
     <TamaguiProvider config={tamaguiConfig}>
       <Suspense>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <YStack>
+          <H1 color={'black'}>Vite + Tamagui</H1>
+          <Button onClick={() => setCount((count) => count + 1)}>count is {count}</Button>
+        </YStack>
       </Suspense>
     </TamaguiProvider>
   )
